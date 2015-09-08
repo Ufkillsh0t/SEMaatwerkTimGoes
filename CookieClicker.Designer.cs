56b6ec5cda94c16f0294e865e7b70a37e3a97949
Ufkillsh0t <Tim_Goes4@hotmail.nl>
2015-09-08 09:54:34 +0200

    UI

---
+++ CookieClicker.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace CookieClicker
+{
+    partial class CookieClicker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CookieClicker));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBakker = new System.Windows.Forms.Button();
+            this.gbxUpgrades = new System.Windows.Forms.GroupBox();
+            this.lblKpsBakker = new System.Windows.Forms.Label();
+            this.btnDeegroller = new System.Windows.Forms.Button();
+            this.btnOven = new System.Windows.Forms.Button();
+            this.lblKpsOven = new System.Windows.Forms.Label();
+            this.lblKpsDeegroller = new System.Windows.Forms.Label();
+            this.lblUKps = new System.Windows.Forms.Label();
+            this.lblUKoekjes = new System.Windows.Forms.Label();
+            this.lblKps = new System.Windows.Forms.Label();
+            this.lblKoekjes = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gbxUpgrades.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 225);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBakker
+            // 
+            this.btnBakker.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBakker.ForeColor = System.Drawing.Color.YellowGreen;
+            this.btnBakker.Location = new System.Drawing.Point(16, 35);
+            this.btnBakker.Name = "btnBakker";
+            this.btnBakker.Size = new System.Drawing.Size(184, 63);
+            this.btnBakker.TabIndex = 1;
+            this.btnBakker.Text = "Bakker (100):";
+            this.btnBakker.UseVisualStyleBackColor = true;
+            // 
+            // gbxUpgrades
+            // 
+            this.gbxUpgrades.Controls.Add(this.lblKpsDeegroller);
+            this.gbxUpgrades.Controls.Add(this.lblKpsOven);
+            this.gbxUpgrades.Controls.Add(this.lblKpsBakker);
+            this.gbxUpgrades.Controls.Add(this.btnDeegroller);
+            this.gbxUpgrades.Controls.Add(this.btnOven);
+            this.gbxUpgrades.Controls.Add(this.btnBakker);
+            this.gbxUpgrades.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxUpgrades.Location = new System.Drawing.Point(244, 54);
+            this.gbxUpgrades.Name = "gbxUpgrades";
+            this.gbxUpgrades.Size = new System.Drawing.Size(329, 291);
+            this.gbxUpgrades.TabIndex = 2;
+            this.gbxUpgrades.TabStop = false;
+            this.gbxUpgrades.Text = "Upgrades";
+            // 
+            // lblKpsBakker
+            // 
+            this.lblKpsBakker.AutoSize = true;
+            this.lblKpsBakker.Location = new System.Drawing.Point(206, 56);
+            this.lblKpsBakker.Name = "lblKpsBakker";
+            this.lblKpsBakker.Size = new System.Drawing.Size(71, 20);
+            this.lblKpsBakker.TabIndex = 4;
+            this.lblKpsBakker.Text = "+ 10 Kps";
+            // 
+            // btnDeegroller
+            // 
+            this.btnDeegroller.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeegroller.ForeColor = System.Drawing.Color.YellowGreen;
+            this.btnDeegroller.Location = new System.Drawing.Point(16, 173);
+            this.btnDeegroller.Name = "btnDeegroller";
+            this.btnDeegroller.Size = new System.Drawing.Size(184, 63);
+            this.btnDeegroller.TabIndex = 3;
+            this.btnDeegroller.Text = "Deegroller (3500) :";
+            this.btnDeegroller.UseVisualStyleBackColor = true;
+            // 
+            // btnOven
+            // 
+            this.btnOven.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOven.ForeColor = System.Drawing.Color.YellowGreen;
+            this.btnOven.Location = new System.Drawing.Point(16, 104);
+            this.btnOven.Name = "btnOven";
+            this.btnOven.Size = new System.Drawing.Size(184, 63);
+            this.btnOven.TabIndex = 2;
+            this.btnOven.Text = "Oven (2000) :";
+            this.btnOven.UseVisualStyleBackColor = true;
+            // 
+            // lblKpsOven
+            // 
+            this.lblKpsOven.AutoSize = true;
+            this.lblKpsOven.Location = new System.Drawing.Point(206, 125);
+            this.lblKpsOven.Name = "lblKpsOven";
+            this.lblKpsOven.Size = new System.Drawing.Size(71, 20);
+            this.lblKpsOven.TabIndex = 5;
+            this.lblKpsOven.Text = "+ 50 Kps";
+            // 
+            // lblKpsDeegroller
+            // 
+            this.lblKpsDeegroller.AutoSize = true;
+            this.lblKpsDeegroller.Location = new System.Drawing.Point(206, 194);
+            this.lblKpsDeegroller.Name = "lblKpsDeegroller";
+            this.lblKpsDeegroller.Size = new System.Drawing.Size(93, 20);
+            this.lblKpsDeegroller.TabIndex = 6;
+            this.lblKpsDeegroller.Text = "+ 10 per klik";
+            // 
+            // lblUKps
+            // 
+            this.lblUKps.AutoSize = true;
+            this.lblUKps.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUKps.Location = new System.Drawing.Point(240, 9);
+            this.lblUKps.Name = "lblUKps";
+            this.lblUKps.Size = new System.Drawing.Size(40, 20);
+            this.lblUKps.TabIndex = 3;
+            this.lblUKps.Text = "Kps:";
+            // 
+            // lblUKoekjes
+            // 
+            this.lblUKoekjes.AutoSize = true;
+            this.lblUKoekjes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUKoekjes.Location = new System.Drawing.Point(240, 29);
+            this.lblUKoekjes.Name = "lblUKoekjes";
+            this.lblUKoekjes.Size = new System.Drawing.Size(69, 20);
+            this.lblUKoekjes.TabIndex = 4;
+            this.lblUKoekjes.Text = "Koekjes:";
+            // 
+            // lblKps
+            // 
+            this.lblKps.AutoSize = true;
+            this.lblKps.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKps.Location = new System.Drawing.Point(330, 9);
+            this.lblKps.Name = "lblKps";
+            this.lblKps.Size = new System.Drawing.Size(16, 18);
+            this.lblKps.TabIndex = 5;
+            this.lblKps.Text = "0";
+            // 
+            // lblKoekjes
+            // 
+            this.lblKoekjes.AutoSize = true;
+            this.lblKoekjes.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKoekjes.Location = new System.Drawing.Point(330, 30);
+            this.lblKoekjes.Name = "lblKoekjes";
+            this.lblKoekjes.Size = new System.Drawing.Size(16, 18);
+            this.lblKoekjes.TabIndex = 6;
+            this.lblKoekjes.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(585, 366);
+            this.Controls.Add(this.lblKoekjes);
+            this.Controls.Add(this.lblKps);
+            this.Controls.Add(this.lblUKoekjes);
+            this.Controls.Add(this.lblUKps);
+            this.Controls.Add(this.gbxUpgrades);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "CookieClicker";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gbxUpgrades.ResumeLayout(false);
+            this.gbxUpgrades.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBakker;
+        private System.Windows.Forms.GroupBox gbxUpgrades;
+        private System.Windows.Forms.Label lblKpsBakker;
+        private System.Windows.Forms.Button btnDeegroller;
+        private System.Windows.Forms.Button btnOven;
+        private System.Windows.Forms.Label lblKpsDeegroller;
+        private System.Windows.Forms.Label lblKpsOven;
+        private System.Windows.Forms.Label lblUKps;
+        private System.Windows.Forms.Label lblUKoekjes;
+        private System.Windows.Forms.Label lblKps;
+        private System.Windows.Forms.Label lblKoekjes;
+    }
+}
+
--- CookieClicker.cs
+++ CookieClicker.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CookieClicker
 {
-    public partial class Form1 : Form
+    public partial class CookieClicker : Form
     {
-        public Form1()
+        public CookieClicker()
         {
             InitializeComponent();
         }
