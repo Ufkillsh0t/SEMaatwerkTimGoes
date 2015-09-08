812f4004a470871e195de1261d0f1eb7f420aa6b
Ufkillsh0t <Tim_Goes4@hotmail.nl>
2015-09-08 10:25:16 +0200

    koekjescontroller

---
+++ KoekjesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieClicker
+{
+    public class KoekjesController
+    {
+        public int Kps { get; set; }
+
+        public int Koekjes { get; set; }
+
+        public int KoekjesPerKlik { get; set; }
+
+        public KoekjesController()
+        {
+            Kps = 0;
+            Koekjes = 0;
+            KoekjesPerKlik = 10;
+        }
+    }
+}
---
+++ Upgrade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieClicker
+{
+    public class Upgrade
+    {
+        public string Naam { get; set; }
+
+        public int Kps { get; set; }
+
+        public int Prijs { get; set; }
+
+        public int PrijsInterval { get; set; }
+
+        public int Aantal { get; set; }
+
+        public Upgrade(string naam, int kps, int prijs, int prijsinterval)
+        {
+            Aantal = 0;
+        }
+    }
+}
